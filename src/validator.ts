fc822d2b65384f2e036fa0e72fae5fb7a230d270
JOAO VICTOR SIMOES CLEMENTIN <joao.clementin@cs.cruzeirodosul.edu.br>
2026-08-19 21:38:05 -0300

    teste envio branch exercicio

--- src/validator.ts
+++ src/validator.ts
@@ -3,6 +3,14 @@ function validarChamado(titulo: string): boolean {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
+    if (titulo.trim().length < 5) {
+        throw new Error("O título deve possuir no mínimo 5 caracteres.");
+    }
+
+    if (titulo.trim().length > 100) {
+        throw new Error("O título deve possuir no máximo 100 caracteres.");
+    }
+
     return true;
 }
 
